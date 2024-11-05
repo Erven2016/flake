d37234b23dcab6bf00b018e696779697e520b330
Guihua Lei <leiguihua2016@gmail.com>
2024-11-05 10:06:39 +0800

    feat(host/EliteBook845G11): enabled amd_pstate

--- flake.nix
+++ flake.nix
@@ -83,7 +83,6 @@
           ./hosts/EliteBook845G11.nix
 
           nixos-hardware.nixosModules.common-cpu-amd
-          # nixos-hardware.nixosModules.common-cpu-amd-pstate
           # nixos-hardware.nixosModules.common-cpu-amd-zenpower
           nixos-hardware.nixosModules.common-gpu-amd
           nixos-hardware.nixosModules.common-hidpi
