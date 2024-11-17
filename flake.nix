d839948563482cfe18ce73909e5a3548a250f0f0
Guihua Lei <leiguihua2016@gmail.com>
2024-11-18 05:09:04 +0800

    chore: updated stateVersion to 24.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "A flake to manage all my machines.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
