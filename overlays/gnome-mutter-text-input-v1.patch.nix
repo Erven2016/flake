23b992325a38150d1f05b406231b70e5cdb451ca
Guihua Lei <leiguihua2016@gmail.com>
2024-11-18 03:41:31 +0800

    refactor（overlays::gnome-mutter-text-input-v1): use gnome attrsets

--- overlays/gnome-mutter-text-input-v1.patch.nix
+++ overlays/gnome-mutter-text-input-v1.patch.nix
@@ -1,23 +1,15 @@
+{ lib, ... }:
 {
   nixpkgs.overlays = [
-    (self: super: {
-      gnome = super.gnome.overrideScope' (
-        pself: psuper: {
-          mutter = psuper.mutter.overrideAttrs (oldAttrs: {
-            patches = (oldAttrs.patches or [ ]) ++ [
-
-              # grab.patch
-              (super.fetchpatch {
-                url = "https://gitlab.gnome.org/GNOME/mutter/-/commit/a99e139a68bd4e5350033163dfd85b6ce6da92a2.patch";
-                hash = "sha256-L8GGEFHE566yvpAAVXA8bn2hvDfFAO8S/MqPM3Oki90=";
-                revert = true;
-              })
-
-              # text-input-v1.patch
-              (super.fetchpatch {
+    (final: prev: {
+      gnome = prev.gnome.overrideScope (
+        gfinal: gprev: {
+          mutter = gprev.mutter.overrideAttrs (oldAttrs: {
+            patches = [
+              (prev.fetchpatch {
                 url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3751/diffs.patch";
-                hash = "sha256-L8GGEFHE566yvpAAVXA8bn2hvDfFAO8S/MqPM3Oki90=";
-                revert = true;
+                hash = "sha256-KOpYon8Ajzn60voOAnj6wqXxZ7nnnJ9RIPhjGBKS5uY=";
+                # revert = true;
               })
             ];
           });
