8ce4cfdf8972bc0efe28e7b41f77e25db38f4bb3
Guihua Lei <leiguihua2016@gmail.com>
2024-11-24 02:20:56 +0800

    feat(python): added pip

--- homeManager/devenv/python/default.nix
+++ homeManager/devenv/python/default.nix
@@ -17,6 +17,7 @@ let
   # It is recommanded to choose a python version which is stable
   # and has a large amount of pythonModules/packages in nixpkgs.
   defaultPythonPackage = pkgs.python312Full;
+  defaultPipPackakge = pkgs.python312Packages.pip;
 
   cfg = config.home.devenv.python;
 in
@@ -28,7 +29,20 @@ in
       type = types.package;
       default = defaultPythonPackage;
     };
+
+    pip = mkOption {
+      type = types.package;
+      default = defaultPipPackakge;
+    };
   };
 
-  config = mkIf (cfg.enable) { home.packages = mkMerge [ [ cfg.package ] ]; };
+  config = mkIf (cfg.enable) {
+    home.packages = mkMerge [
+      [
+        cfg.package
+        cfg.pip
+        pkgs.pipx
+      ]
+    ];
+  };
 }
