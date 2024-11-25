2c1086199aee8b1a4e429af02bbfd9028a441995
Guihua Lei <leiguihua2016@gmail.com>
2024-11-26 06:24:44 +0800

    fix: wrong path of evalHost.nix  to import

--- utils/genHomeTargets.nix
+++ utils/genHomeTargets.nix
@@ -3,7 +3,7 @@ lib.flatten (
   lib.forEach (builtins.attrNames (builtins.readDir hostsDir)) (
     hostname:
     let
-      users = (import ../utils/evalHost.nix { inherit lib hostsDir hostname; }).users;
+      users = (import ./evalHost.nix { inherit lib hostsDir hostname; }).users;
     in
     lib.forEach users (username: "${username}@${hostname}")
   )
