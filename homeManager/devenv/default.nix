3958602af682f41429deb4fb316d9738ff5cfa74
Guihua Lei <leiguihua2016@gmail.com>
2024-11-30 09:54:11 +0800

    revert: "feat: added devenv to home-manager config"
    This reverts commit 9ad166e38bcaeb1e94f30f7a9f3902e3d9c57dcb.

--- homeManager/devenv/default.nix
+++ homeManager/devenv/default.nix
@@ -1,32 +1,8 @@
 {
-  pkgs,
-  lib,
-  config,
-  ...
-}:
-let
-  inherit (lib) mkEnableOption mkIf;
-
-  cfg = config.home.devenv;
-in
-{
   imports = [
     ./nodejs
     ./rust
     ./python
     ./go
   ];
-
-  options.home.devenv.enable = mkEnableOption "devenv";
-
-  config = {
-    home.packages = mkIf (cfg.enable) ([ pkgs.unstable.devenv ]);
-
-    # to use cache nix
-    nix.extraOptions = mkIf (cfg.enable) ''
-      trusted-users = root ${config.home.username}
-      extra-substituters = https://devenv.cachix.org
-      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
-    '';
-  };
 }
