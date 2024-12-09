f43fca0a5ab230ed1fb26b557155e63395599920
erven2016 <leiguihua2016@gmail.com>
2024-12-09 20:22:13 +0800

    chore(erven2016): disabled zed-editor

--- users/erven2016/home.nix
+++ users/erven2016/home.nix
@@ -15,7 +15,7 @@
 
   home.programs.zsh.enable = true;
   home.programs.joshuto.enable = true;
-  home.programs.zed-editor.enable = true;
+  # home.programs.zed-editor.enable = true;
   programs.zsh.oh-my-zsh.plugins = [ "rust" ];
   home.programs.direnv.enable = true;
 
