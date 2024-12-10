5b382b9b56d18fba5daa69fc5b4509351950843d
erven2016 <leiguihua2016@gmail.com>
2024-12-10 13:58:51 +0800

    chore: rm zsh integration of joshuto

--- nixos/optional/joshuto/_home.nix
+++ nixos/optional/joshuto/_home.nix
@@ -18,16 +18,13 @@ in
   options.home.programs.joshuto = {
     enable = mkEnableOption "joshuto";
 
-    enableZshIntegration = mkEnableOption "joshuto integration for ZSH" // {
-      default = true;
-    };
     integrationKeybind = mkOption {
       type = types.str;
       description = "keys to open joshuto, defaultly are `Alt`-`e`";
       default = "^[e"; # Alt-e
     };
-    enableUnstableVersion = mkEnableOption "joshotu version of nixpkgs-unstable";
 
+    enableUnstableVersion = mkEnableOption "joshotu version of nixpkgs-unstable";
   };
 
   config = mkIf cfg.enable {
@@ -35,20 +32,5 @@ in
       enable = cfg.enable;
       package = mkIf (cfg.enableUnstableVersion) pkgs.unstable.joshuto;
     };
-
-    programs.zsh.initExtra =
-      mkIf (cfg.enableZshIntegration && cfg.enable && config.programs.zsh.enable)
-        ''
-          _joshuto_wrapper() {
-            BUFFER="joshuto ."
-            zle accept-line
-            zle reset-prompt
-          }
-
-          zle -N _joshuto_wrapper
-
-
-          bindkey "${cfg.integrationKeybind}" _joshuto_wrapper
-        '';
   };
 }
