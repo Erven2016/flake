519214f56a16d15d28108eb93ad7411bcea21595
erven2016 <leiguihua2016@gmail.com>
2024-12-12 21:03:31 +0800

    chore(zed-editor): move keymappings to a new file

--- nixos/optional/zed-editor/home.nix
+++ nixos/optional/zed-editor/home.nix
@@ -11,12 +11,13 @@ let
     mkIf
     mkDefault
     mkForce
-    mkBefore
     ;
 
   cfg = config.home.programs.zed-editor;
 in
 {
+  imports = [ ./keymap/home.nix ];
+
   options.home.programs.zed-editor = {
     enable = mkEnableOption "zed-editor";
     enableUnstableVersion = mkEnableOption "unstable version of zed-editor" // {
@@ -125,53 +126,6 @@ in
         "catppuccin" # Theme
         "nix" # nix language support
         "env"
-      ];
-
-      userKeymaps = mkBefore [
-        # In normal & visual mode
-        {
-          context = "Editor && (vim_mode == normal || vim_mode == visual)";
-          bindings = {
-            # Helix-like keymappings
-            "g g" = "vim::StartOfDocument";
-            "g e" = "vim::EndOfDocument";
-            "g h" = "vim::StartOfLine";
-            "g l" = "vim::EndOfLine";
-            "space c" = "vim::ToggleComments";
-            "U" = "vim::Redo";
-
-            # Custom keymappings
-            "ctrl-j" = [
-              "workspace::SendKeystrokes"
-              "down down down down"
-            ];
-            "ctrl-k" = [
-              "workspace::SendKeystrokes"
-              "up up up up"
-            ];
-          };
-        }
-
-        # In normal mode
-        {
-          context = "Editor && vim_mode == normal";
-          bindings = {
-            # Helix like keymappings
-            "space f" = "file_finder::Toggle";
-
-            # format buffer
-            "space i" = "editor::Format";
-            # global search
-            "space /" = "workspace::NewSearch";
-            # go to line
-            "space g" = "go_to_line::Toggle";
-          };
-        }
-        {
-          bindings = {
-            "alt-z" = "workspace::ToggleZoom";
-          };
-        }
       ];
     };
 
---
+++ nixos/optional/zed-editor/keymap/home.nix
@@ -0,0 +1,60 @@
+{ lib, options, ... }:
+let
+  inherit (lib) mkIf mkEnableOption;
+
+  cfg = options.home.programs.zed-editor;
+in
+{
+  options.home.programs.zed-editor = {
+    enableHelixKeymap = mkEnableOption "helix-like keymappings for zed-editor";
+  };
+  config = mkIf cfg.enableHelixKeymap {
+    programs.zed-editor.userKeymaps = [
+      # In normal & visual mode
+      {
+        context = "Editor && (vim_mode == normal || vim_mode == visual)";
+        bindings = {
+          # Helix-like keymappings
+          "g g" = "vim::StartOfDocument";
+          "g e" = "vim::EndOfDocument";
+          "g h" = "vim::StartOfLine";
+          "g l" = "vim::EndOfLine";
+          "space c" = "vim::ToggleComments";
+          "U" = "vim::Redo";
+
+          # Custom keymappings
+          "ctrl-j" = [
+            "workspace::SendKeystrokes"
+            "down down down down"
+          ];
+          "ctrl-k" = [
+            "workspace::SendKeystrokes"
+            "up up up up"
+          ];
+        };
+      }
+
+      # In normal mode
+      {
+        context = "Editor && vim_mode == normal";
+        bindings = {
+          # Helix like keymappings
+          "space f" = "file_finder::Toggle";
+
+          # format buffer
+          "space i" = "editor::Format";
+          # global search
+          "space /" = "workspace::NewSearch";
+          # go to line
+          "space g" = "go_to_line::Toggle";
+        };
+      }
+      {
+        bindings = {
+          # Zen Mode
+          "alt-z" = "workspace::ToggleZoom";
+        };
+      }
+    ];
+  };
+}
